86fd1df46bbdab27f24ec01d3e5af63367a1991f
AndrewNduati <andrew.njage@meltwater.org>
2017-11-13 17:56:51 +0300

    Added controller to handle POST requests from the bot

--- test/controllers/conversion_controller_test.rb
+++ test/controllers/conversion_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class ConvertionControllerTest < ActionDispatch::IntegrationTest
+class ConversionControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
